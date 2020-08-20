62fcc702fd3d9ed71410c4103f3d907e7d143476
John Bowser <john.bow7720@gmail.com>
2020-08-20 00:35:03 -0600

    intial setup/ added GET/articles and GET/articles/:article_id w/ tests

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
 
 const app = express()
 
@@ -14,6 +16,24 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.get('/articles', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getAllArticles(knexInstance)
+    .then(articles => {
+      res.json(articles)
+    })
+    .catch(next)
+})
+
+app.get('/articles/:article_id', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getById(knexInstance, req.params.article_id)
+    .then(article => {
+      res.json(article)
+    })
+    .catch(next)
+})
 
 app.get('/', (req, res) => {
   res.send('Hello, boilerplate!')
